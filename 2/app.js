37b0385ccfe21d7e114427e006c7f0ea5e54300e
Yeni0325 <yeeun940325@gmail.com>
2022-01-19 01:24:16 +0900

    자바스크립트 퀴즈

--- 2/app.js
+++ 2/app.js
@@ -3,18 +3,31 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 // String만 포함된 변수는 대문자로 표기한다!!
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
     
     //greeting.innerText = "Hello " + username;
-    greeting.innerText = `Hello ${username}`;
+    paintGreetings();
+}
 
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+const saveUsername = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener("submit",onLoginSubmit);
+if(saveUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+} else {
+    // show the greetings
+    paintGreetings();
+}
