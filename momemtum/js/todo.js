9bf666995939cad8b9be15e388c91137d531997d
Yeni0325 <yeeun940325@gmail.com>
2022-01-26 22:51:51 +0900

    자바스크립트 연습

--- momemtum/js/todo.js
+++ momemtum/js/todo.js
@@ -2,12 +2,15 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");   // = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo){
+    console.log("i will paint", newTodo);
+}
+
 function handleTodoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
-    console.log(toDoInput.value);
     toDoInput.value = "";              // toDoInput.value에 빈 값을 넣었다고 해서 newTodo에 담긴 값이 빈 문자열이 담긴것이 아님!!
-    console.log(newTodo, toDoInput.value);
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit);
